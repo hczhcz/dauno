74f8f89a07eff62bf3cd1dcd95d2b1756d9c8aab
hcz <h.cz@qq.com>
2015-07-19 16:29:17 +0800

    working

--- dauno_client.js
+++ dauno_client.js
@@ -1,30 +1,30 @@
 'use strict';
 
 var https = require('https');
-var crypto = require('crypto');
 var io = require('socket.io-client');
 
-var sha256 = function (data) {
-    return crypto.createHash('sha256').update(data).digest('hex')
-};
+var dauno = require('./dauno');
 
-var host = 'https://127.0.0.1:8001';
-var login = {
-    username: 'test',
-    password: sha256('test'),
-};
+var makeClient = function (host, name, password) {
+    var socket = io(host);
 
-var socket = io(host);
+    socket.on('connect', function () {
+        dauno.taskLog('Connect');
 
-socket.on('connect', function () {
-    console.log(new Date() + ' connect');
-    socket.emit('login', login);
-});
-
-socket.on('req', function (data) {
-    console.log(data);
-
-    socket.emit('res', {
-        test: 'test2',
+        socket.emit('login', {
+            name: name,
+            password: dauno.hash(password),
+        });
     });
-});
+
+    socket.on('req', function (data) {
+        dauno.taskLog('Request', JSON.stringify(data));
+
+        // TODO
+        socket.emit('res', {
+            test: 'test2',
+        });
+    });
+};
+
+makeClient('https://127.0.0.1:8001', 'test', 'pass');
--- dauno_server.js
+++ dauno_server.js
@@ -6,26 +6,38 @@ var fs = require('fs');
 var https = require('https');
 var io = require('socket.io');
 
+var dauno = require('./dauno');
+
 var handlers = {
     app: function (req, res) {
-        console.log(req);
+        dauno.httpLog(
+            req.connection, 'REQ', '/app' + req.url
+        );
 
         var data = {
             method: req.method,
             url: req.url,
             headers: req.headers,
             trailers: req.trailers,
-            now: new Date(),
+            now: dauno.date(),
         };
 
         res.writeHead(200);
         res.end(JSON.stringify(data));
     },
     err404: function (req, res) {
+        dauno.httpLog(
+            req.connection, 404, req.url
+        );
+
         res.writeHead(404);
         res.end();
     },
     err500: function (req, res) {
+        dauno.httpLog(
+            req.connection, 500, req.url
+        );
+
         res.writeHead(500);
         res.end();
     },
@@ -46,6 +58,8 @@ var handlerMain = function (req, res) {
 
         return handlers['err404'](req, res);
     } catch (e) {
+        dauno.errLog(String(e));
+
         return handlers['err500'](req, res);
     }
 };
@@ -68,10 +82,10 @@ socketServer.on('connection', function (socket) {
     });
 
     socket.on('login', function (data) {
-        console.log(data);
+        dauno.sockLog(socket.conn.remoteAddress, 'Login', JSON.stringify(data));
     });
 
     socket.on('res', function (data) {
-        console.log(data);
+        dauno.sockLog(socket.conn.remoteAddress, 'Response', JSON.stringify(data));
     });
 });
