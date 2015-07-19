b5837b4d59eb5fa485ce0059697e6a4032249586
hcz <h.cz@qq.com>
2015-07-19 16:29:13 +0800

    add dauno.js

---
+++ dauno.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var crypto = require('crypto');
+
+module.exports.date = function () {
+    return Date.now();
+};
+
+var printLog = function (info) {
+    var date = new Date();
+
+    console.log(
+        '['
+            + date.getFullYear() + '.'
+            + (date.getMonth() + 1) + '.'
+            + date.getDate() + ' '
+            + date.getHours() + ':'
+            + date.getMinutes() + ':'
+            + date.getSeconds() + '.'
+            + date.getMilliseconds()
+        + '] '
+        + info
+    );
+};
+
+module.exports.taskLog = function (task, info) {
+    if (info) {
+        printLog(task + ': ' + info);
+    } else {
+        printLog(task);
+    }
+};
+
+module.exports.sockLog = function (ip, task, info) {
+    printLog('[' + ip + '] ' + task + ': ' + info);
+};
+
+module.exports.httpLog = function (conn, status, path) {
+    printLog(
+        '[' + conn.remoteAddress + ':' + conn.remotePort + '] [' + status + '] '
+        + path
+    );
+};
+
+module.exports.errLog = function (info) {
+    printLog('ERROR: ' + info);
+};
+
+module.exports.hash = function (data) {
+    return crypto.createHash('sha256').update(data).digest('hex');
+};
